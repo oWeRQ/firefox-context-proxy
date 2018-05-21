332e78f13aa53876621f6d23988d26574fe7199b
Maxim Soldatov <ms@nowmedia.ru>
2018-05-21 19:40:23 +0300

    reimplement save proxies

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
-var proxyByContainer = {};
+var proxyByStore = {};
 
-browser.storage.local.get().then(function(items){
-	proxyByContainer = items;
+browser.storage.local.get('proxyByStore').then(function(items){
+	proxyByStore = items.proxyByStore;
 });
 
 browser.storage.onChanged.addListener(function(changes, areaName){
-	for (let name in changes) {
-		proxyByContainer[name] = changes[name].newValue;
+	if ('proxyByStore' in changes) {
+		proxyByStore = changes.proxyByStore.newValue;
 	}
 });
 
 browser.proxy.onRequest.addListener(function(requestInfo) {
 	return new Promise(function(resolve, reject) {
 		browser.tabs.get(requestInfo.tabId)
-			.then(tab => resolve(proxyByContainer[tab.cookieStoreId]))
+			.then(tab => resolve(proxyByStore[tab.cookieStoreId]))
 			.catch(() => resolve({type: 'direct'}));
 	});
 }, {urls: ['<all_urls>']});
--- context.js
+++ context.js
@@ -1,34 +1,3 @@
-/*
-function eventHandler(event) {
-	if (event.target.dataset.action == 'create') {
-		browser.tabs.create({
-			url: 'about:blank',
-			cookieStoreId: event.target.dataset.identity
-		});
-	}
-	if (event.target.dataset.action == 'close-all') {
-		browser.tabs.query({
-			cookieStoreId: event.target.dataset.identity
-		}).then((tabs) => {
-			browser.tabs.remove(tabs.map((i) => i.id));
-		});
-	}
-	event.preventDefault();
-}
-
-function createOptions(node, identity) {
-	for (let option of ['Create', 'Close All']) {
-		let a = document.createElement('a');
-		a.href = '#';
-		a.innerText = option;
-		a.dataset.action = option.toLowerCase().replace(' ', '-');
-		a.dataset.identity = identity.cookieStoreId;
-		a.addEventListener('click', eventHandler);
-		node.appendChild(a);
-	}
-}
-*/
-
 function createIdentityHead(identity) {
 	let head = document.createElement('div');
 	head.className = 'head';
@@ -42,9 +11,8 @@ function createIdentityHead(identity) {
 	return head;
 }
 
-function createProxyForm(identity) {
+function createProxyForm(identity, values) {
 	var form = document.createElement('form');
-	form.dataset.identity = identity.cookieStoreId;
 
 	let typeSelect = document.createElement('select');
 	typeSelect.name = 'type';
@@ -54,15 +22,18 @@ function createProxyForm(identity) {
 		typeOption.innerText = type;
 		typeSelect.appendChild(typeOption);
 	}
+	typeSelect.value = values.type || 'direct';
 	form.appendChild(typeSelect);
 
 	let hostInput = document.createElement('input');
 	hostInput.name = 'host';
+	hostInput.value = values.host || '';
 	hostInput.placeholder = 'Host';
 	form.appendChild(hostInput);
 
 	let portInput = document.createElement('input');
 	portInput.name = 'port';
+	portInput.value = values.port || '';
 	portInput.placeholder = 'Port';
 	form.appendChild(portInput);
 
@@ -71,47 +42,46 @@ function createProxyForm(identity) {
 	saveButton.innerText = 'Save';
 	form.appendChild(saveButton);
 
-	form.addEventListener('submit', function(e){
-		e.preventDefault();
-		browser.storage.local.set({
-			[identity.cookieStoreId]: {
-				type: form.elements.type.value,
-				host: form.elements.host.value,
-				port: form.elements.port.value,
-			}
-		});
-	});
-
-	browser.storage.local.get(identity.cookieStoreId).then(function(items){
-		let values = items[identity.cookieStoreId];
-		for (let name in values) {
-			form.elements[name].value = values[name];
-		}
-	});
-
 	return form;
 }
 
-var div = document.getElementById('identity-list');
+var list = document.getElementById('identity-list');
 
 if (browser.contextualIdentities === undefined) {
-	div.innerText = 'browser.contextualIdentities not available. Check that the privacy.userContext.enabled pref is set to true, and reload the add-on.';
+	list.innerText = 'browser.contextualIdentities not available. Check that the privacy.userContext.enabled pref is set to true, and reload the add-on.';
 } else {
-	browser.contextualIdentities.query({})
-		.then((identities) => {
-			if (!identities.length) {
-				div.innerText = 'No identities returned from the API.';
-				return;
-			}
+	Promise.all([
+		browser.contextualIdentities.query({}),
+		browser.storage.local.get('proxyByStore')
+	]).then(([identities, items]) => {
+		let proxyByStore = items.proxyByStore || {};
 
-		 for (let identity of identities) {
+		if (!identities.length) {
+			list.innerText = 'No identities returned from the API.';
+			return;
+		}
+
+		for (let identity of identities) {
+			let form = createProxyForm(identity, proxyByStore[identity.cookieStoreId] || {});
+			form.addEventListener('submit', e => {
+				e.preventDefault();
+
+				proxyByStore[identity.cookieStoreId] = {
+					type: form.elements.type.value,
+					host: form.elements.host.value,
+					port: form.elements.port.value,
+				};
+
+				browser.storage.local.set({proxyByStore});
+			});
+
 			let row = document.createElement('div');
 			row.className = 'row';
 
 			row.appendChild(createIdentityHead(identity));
-			row.appendChild(createProxyForm(identity));
+			row.appendChild(form);
 
-			div.appendChild(row);
-		 }
+			list.appendChild(row);
+		}
 	});
 }
